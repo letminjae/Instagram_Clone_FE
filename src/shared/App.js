343af1326a996888ec3d891b539016e220b96627
letminjae <cmjj0824@naver.com>
2022-02-18 18:55:07 +0900

    라우팅 완료

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import React from 'react';
-
-import Main from '../pages/Main'
-import Signin from '../pages/Signin'
-import Signup from '../pages/Signup'
-import Direct from '../pages/Direct'
-import Mypage from '../pages/Mypage'
-
 import { history } from '../redux/configureStore'
 import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router-dom';
+
+import Signin from '../pages/Signin'
+import Signup from '../pages/Signup';
+import Mypage from '../pages/Mypage';
+import Direct from '../pages/Direct';
+import Main from '../pages/Main'
 
 function App() {
   return (
     <React.Fragment>
       <ConnectedRouter history={history}>
-        <Route path='/direct' component={Direct} />
-        <Route path='/signup' component={Signup} />
-        <Route path='/signin' component={Signin} />
-        <Route path='/mypage' component={Mypage} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/mypage" component={Mypage} />
+        <Route path="/direct" component={Direct} />
         <Route path='/' exact component={Main} />
       </ConnectedRouter>
     </React.Fragment>
