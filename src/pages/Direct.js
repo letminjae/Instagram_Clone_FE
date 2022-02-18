021df5fe3a46e5e2f6230be000d72cab6ff43a9b
letminjae <cmjj0824@naver.com>
2022-02-18 18:20:52 +0900

    element 수정

---
+++ src/pages/Direct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Direct = () => {
+    
+    return(
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default Direct
---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = () => {
+    
+    return(
+        <React.Fragment>
+            회원가입
+        </React.Fragment>
+    )
+}
+
+export default Signup
